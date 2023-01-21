08f27c76f6b917ed18fdb8d1b9c957a242a4a2f8
Jeong Min Choi <tangerine_t@naver.com>
2023-01-21 18:02:01 +0900

    Update Texture

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
 
-    import * as THREE from 'three';
+import * as THREE from 'three';
 
-    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-    import { Water } from 'three/addons/objects/Water2.js';
-    import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { Water } from 'three/addons/objects/Water2.js';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 
-    let scene, camera, clock, renderer, water;
+let scene, camera, clock, renderer, water;
 
-    let torusKnot;
+let torusKnot;
 
-    const params = {
+const params = {
     color: '#bdebff',
     scale: 16,
     flowX: -1,
     flowY: 1
-    };
+};
 
-    init();
-    animate();
+init();
+animate();
 
-    function init() {
+function init() {
 
     // scene
 
@@ -50,7 +50,7 @@
     scene.add( ground );
 
     const textureLoader = new THREE.TextureLoader();
-    textureLoader.load( 'textures/clear_sea.png', function ( map ) {
+    textureLoader.load( 'examples/textures/clear_sea.png', function ( map ) {
 
     map.wrapS = THREE.RepeatWrapping;
     map.wrapT = THREE.RepeatWrapping;
@@ -59,142 +59,142 @@
     groundMaterial.map = map;
     groundMaterial.needsUpdate = true;
 
-    } );
+} );
 
-    // water
+// water
 
-    const waterGeometry = new THREE.PlaneGeometry( 4096,4096 );
+const waterGeometry = new THREE.PlaneGeometry( 4096,4096 );
 
-    water = new Water( waterGeometry, {
+water = new Water( waterGeometry, {
     color: params.color,
     scale: params.scale,
     flowDirection: new THREE.Vector2( params.flowX, params.flowY ),
     textureWidth: 1024,
     textureHeight: 1024
-    } );
+} );
 
-    water.position.y = 3;
-    water.rotation.x = Math.PI * - 0.5;
-    scene.add( water );
+water.position.y = 3;
+water.rotation.x = Math.PI * - 0.5;
+scene.add( water );
 
-    // skybox
+// skybox
 
-    const cubeTextureLoader = new THREE.CubeTextureLoader();
-    cubeTextureLoader.setPath( 'examples/textures/cube/Park2/' );
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+cubeTextureLoader.setPath( 'examples/textures/cube/Park2/' );
 
-    const cubeTexture = cubeTextureLoader.load( [
-        'px.png', 'nx.png',
-        'py.png', 'ny.png',
-        'pz.png', 'nz.png'
-    ] );
+const cubeTexture = cubeTextureLoader.load( [
+    'px.png', 'nx.png',
+    'py.png', 'ny.png',
+    'pz.png', 'nz.png'
+] );
 
-    scene.background = cubeTexture;
-    scene.rotation.set(0.0, 0.0, 0.0);
+scene.background = cubeTexture;
+scene.rotation.set(0.0, 0.0, 0.0);
 
-    // light
+// light
 
-    const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.6 );
-    scene.add( ambientLight );
+const ambientLight = new THREE.AmbientLight( 0xcccccc, 0.6 );
+scene.add( ambientLight );
 
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.82 );
-    directionalLight.position.set( - 1, 1, 1 );
-    scene.add( directionalLight );
+const directionalLight = new THREE.DirectionalLight( 0xffffff, 0.82 );
+directionalLight.position.set( - 1, 1, 1 );
+scene.add( directionalLight );
 
-    // gltf
+// gltf
 
-    const loader = new GLTFLoader();
-    loader.load( 'data/hazel/Hazel.glb', function ( gltf ) {
-    // Hazel
-    gltf.scene.position.set(0.0, 4.0, 0.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+const loader = new GLTFLoader();
+loader.load( 'data/hazel/Hazel.glb', function ( gltf ) {
+// Hazel
+gltf.scene.position.set(0.0, 4.0, 0.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
-    loader.load( 'data/luke/Luke.glb', function ( gltf ) {
+loader.load( 'data/luke/Luke.glb', function ( gltf ) {
 
-    gltf.scene.position.set(0.0, 4.0, 10.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+gltf.scene.position.set(0.0, 4.0, 10.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
-    loader.load( 'data/orca_army/Orca_Army.glb', function ( gltf ) {
+loader.load( 'data/orca_army/Orca_Army.glb', function ( gltf ) {
 
-    gltf.scene.position.set(0.0, 4.0, -20.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+gltf.scene.position.set(0.0, 4.0, -20.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
-    loader.load( 'data/finn/Finn.glb', function ( gltf ) {
+loader.load( 'data/finn/Finn.glb', function ( gltf ) {
 
-    gltf.scene.position.set(0.0, 4.0, -10.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+gltf.scene.position.set(0.0, 4.0, -10.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
-    loader.load( 'data/moby/Moby.glb', function ( gltf ) {
+loader.load( 'data/moby/Moby.glb', function ( gltf ) {
 
-    gltf.scene.position.set(0.0, 4.0, 20.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+gltf.scene.position.set(0.0, 4.0, 20.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
 
-    loader.load( 'data/bottle/Bottle.glb', function ( gltf ) {
+loader.load( 'data/bottle/Bottle.glb', function ( gltf ) {
 
-    gltf.scene.position.set(0.0, 4.0, -30.0);
-    gltf.scene.rotation.set(0.0, 0.0, 0.0);
-    scene.add( gltf.scene );
-    render();
+gltf.scene.position.set(0.0, 4.0, -30.0);
+gltf.scene.rotation.set(0.0, 0.0, 0.0);
+scene.add( gltf.scene );
+render();
 
-    } );
+} );
 
-    // renderer
+// renderer
 
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    document.body.appendChild( renderer.domElement );
+renderer = new THREE.WebGLRenderer( { antialias: true } );
+renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setPixelRatio( window.devicePixelRatio );
+document.body.appendChild( renderer.domElement );
 
-    //
+//
 
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.minDistance = 5;
-    controls.maxDistance = 50;
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.minDistance = 5;
+controls.maxDistance = 50;
 
-    //
+//
 
-    window.addEventListener( 'resize', onWindowResize );
+window.addEventListener( 'resize', onWindowResize );
 
-    }
+}
 
-    function onWindowResize() {
+function onWindowResize() {
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    }
+}
 
-    function animate() {
+function animate() {
 
     requestAnimationFrame( animate );
     render();
 
-    }
+}
 
-    function render() {
+function render() {
 
     const delta = clock.getDelta();
     renderer.render( scene, camera );
 
-    }
+}
